a4736d4dd241f2f82dfd5f1ef8d809bbefec5dfc
Your Name <you@example.com>
2017-07-26 12:50:27 +0200

    [TASK] Add Classic Bookmark support

--- src/code/09_malBookmarks.js
+++ src/code/09_malBookmarks.js
@@ -4,9 +4,16 @@
         }
         $(window).load(function(){
             var checkExist = setInterval(function() {
-                if ($('.list-item').first().length){
+                if ($('.list-item').first().length || $('.header_cw').first().length){
                     clearInterval(checkExist);
                     var url = '';
+                    //Classic List formating
+                    $('#list_surround table td[class^="td"]:last-child').addClass("tags");  //.css('background-color','red');
+                    $('#list_surround table').addClass("list-table-data");
+                    $('#list_surround table td[class^="td"]:first-child').addClass("title").addClass("data");
+                    $('#list_surround table .animetitle').addClass("link");
+                    $('#list_surround table td[class^="td"]:nth-last-child(2)').addClass("progress").addClass("data").find('span a').addClass('link');
+                    //
                     $('.tags span a').each(function( index ) {
                         if($(this).text().indexOf("last::") > -1 ){
                             url = $(this).text().split("last::")[1].split("::")[0];
@@ -22,7 +29,11 @@
                                     $(this).closest('.list-table-data').find('.stream').after('<span class="content-status"><a class="nextStream" title="Next Episode" target="_blank" style="margin: 0 5px; color: #BABABA;" href="'+ GM_getValue( url+'/nextEp')+'">'+'Next Episode'+'</a></span>');
                                 }
                             }
-                            $(this).parent().remove();
+                            if($('#list_surround').length){
+                                $(this).remove();
+                            }else{
+                                $(this).parent().remove();
+                            }
                         }
                     });
                     return true;
