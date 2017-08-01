6dab72a48c389a1cd5bd54a51b5acf61b1cefce9
Your Name <you@example.com>
2017-08-01 15:26:40 +0200

    [TASK] Redo UI Spacing

--- src/code/06_checkdata.js
+++ src/code/06_checkdata.js
@@ -6,44 +6,23 @@
         }
 
         $.docReady(function() {
-            if(listType == 'anime'){
-                var spacer = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp';
-                var middle = '';
-                middle += '<span class="info">Episodes: </span>';
-                middle += '<span style="color: '+textColor+'; text-decoration: none; outline: medium none;">';
-                middle += '<input id="malEpisodes" value="0" style="background: transparent; border-width: 1px; border-color: grey; text-align: right; color: '+textColor+'; text-decoration: none; outline: medium none;" type="text" size="1" maxlength="4">';
-                middle += '/<span id="malTotal">0</span>';
-                middle += '</span>';
+            var wrapStart = '<span style="display: inline-block;">';
+            var wrapEnd = '</span>';
 
-            }else{
-                var spacer = '&nbsp;&nbsp;&nbsp;&nbsp';
-                var middle = '';
-                middle += '<span class="info">Volumes: </span>';
-                middle += '<span style="color: '+textColor+'; text-decoration: none; outline: medium none;">';
-                middle += '<input id="malVolumes" value="0" style="background: transparent; border-width: 1px; border-color: grey; text-align: right; color: '+textColor+'; text-decoration: none; outline: medium none;" type="text" size="1" maxlength="4">';
-                middle += '/<span id="malTotalVol">0</span>';
-                middle += '</span>';
-
-                middle += spacer;
-
-                middle += '<span class="info">Chapters: </span>';
-                middle += '<span style="color: '+textColor+'; text-decoration: none; outline: medium none;">';
-                middle += '<input id="malChapters" value="0" style="background: transparent; border-width: 1px; border-color: grey; text-align: right; color: '+textColor+'; text-decoration: none; outline: medium none;" type="text" size="1" maxlength="4">';
-                middle += '/<span id="malTotalCha">0</span>';
-                middle += '</span>';
-            }
             var ui = '<p id="malp">';
             ui += '<span id="MalInfo">Loading</span>';
 
-            ui += '<span id="MalData" style="display: none;">';
+            ui += '<span id="MalData" style="display: none; justify-content: space-between;">';
 
+            ui += wrapStart;
             ui += '<span class="info">Mal Score: </span>';
             ui += '<a id="malRating" style="color: '+textColor+';min-width: 30px;display: inline-block;" target="_blank" href="">____</a>';
+            ui += wrapEnd;
 
-            ui += '<span id="MalLogin">';
+            //ui += '<span id="MalLogin">';
+            wrapStart = '<span style="display: inline-block; display: none;" class="MalLogin">';
 
-            ui += spacer;
-
+            ui += wrapStart;
             ui += '<span class="info">Status: </span>';
             ui += '<select id="malStatus" style="font-size: 12px;background: transparent; border-width: 1px; border-color: grey; color: '+textColor+'; text-decoration: none; outline: medium none;">';
             //ui += '<option value="0" style="background: #111111;color: '+textColor+';"></option>';
@@ -53,15 +32,43 @@
             ui += '<option value="4" style="background: #111111;color: '+textColor+';">Dropped</option>';
             ui += '<option value="6" style="background: #111111;color: '+textColor+';">'+planTo+'</option>';
             ui += '</select>';
+            ui += wrapEnd;
 
-            ui += spacer;
+            if(listType == 'anime'){
+                var middle = '';
+                middle += wrapStart;
+                middle += '<span class="info">Episodes: </span>';
+                middle += '<span style="color: '+textColor+'; text-decoration: none; outline: medium none;">';
+                middle += '<input id="malEpisodes" value="0" style="background: transparent; border-width: 1px; border-color: grey; text-align: right; color: '+textColor+'; text-decoration: none; outline: medium none;" type="text" size="1" maxlength="4">';
+                middle += '/<span id="malTotal">0</span>';
+                middle += '</span>';
+                middle += wrapEnd;
+
+            }else{
+                var middle = '';
+                middle += wrapStart;
+                middle += '<span class="info">Volumes: </span>';
+                middle += '<span style="color: '+textColor+'; text-decoration: none; outline: medium none;">';
+                middle += '<input id="malVolumes" value="0" style="background: transparent; border-width: 1px; border-color: grey; text-align: right; color: '+textColor+'; text-decoration: none; outline: medium none;" type="text" size="1" maxlength="4">';
+                middle += '/<span id="malTotalVol">0</span>';
+                middle += '</span>';
+                middle += wrapEnd;
+
+
+                middle += wrapStart;
+                middle += '<span class="info">Chapters: </span>';
+                middle += '<span style="color: '+textColor+'; text-decoration: none; outline: medium none;">';
+                middle += '<input id="malChapters" value="0" style="background: transparent; border-width: 1px; border-color: grey; text-align: right; color: '+textColor+'; text-decoration: none; outline: medium none;" type="text" size="1" maxlength="4">';
+                middle += '/<span id="malTotalCha">0</span>';
+                middle += '</span>';
+                middle += wrapEnd;
+            }
 
             ui += middle;
 
-            ui += spacer;
 
+            ui += wrapStart;
             ui += '<span class="info">Your Score: </span>';
-
             ui += '<select id="malUserRating" style="font-size: 12px;background: transparent; border-width: 1px; border-color: grey; color: '+textColor+'; text-decoration: none; outline: medium none;"><option value="" style="background: #111111;color: '+textColor+';">Select</option>';
             ui += '<option value="10" style="background: #111111;color: '+textColor+';">(10) Masterpiece</option>';
             ui += '<option value="9" style="background: #111111;color: '+textColor+';">(9) Great</option>';
@@ -74,7 +81,9 @@
             ui += '<option value="2" style="background: #111111;color: '+textColor+';">(2) Horrible</option>';
             ui += '<option value="1" style="background: #111111;color: '+textColor+';">(1) Appalling</option>';
             ui += '</select>';
-            ui += '</span>';
+            ui += wrapEnd;
+
+            //ui += '</span>';
             ui += '</span>';
             ui += '</p>';
 
--- src/kissanimelist.user.js
+++ src/kissanimelist.user.js
@@ -829,7 +829,7 @@ if (window.top != window.self) {return; }
     }
 
     function handleanime(anime){
-        $('#MalLogin').css("display","initial");
+        $('.MalLogin').css("display","initial");
         $('#AddMalDiv').remove();
 
         $(".open-info-popup").unbind('click').show().click( function(){
@@ -849,8 +849,8 @@ if (window.top != window.self) {return; }
             }catch(e){}
         }
         if(anime['login'] === 0){
-            $('#MalLogin').css("display","none");
-            $("#MalData").css("display","initial");
+            $('.MalLogin').css("display","none");
+            $("#MalData").css("display","flex");
             $("#MalInfo").text("");
             $("#malRating").attr("href", anime['malurl']);
             $("#malRating").after("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Please log in on <a target='_blank' href='https://myanimelist.net/login.php'>MyAnimeList!<a>");
@@ -861,7 +861,7 @@ if (window.top != window.self) {return; }
             $("#flash").attr("anime", anime['.'+listType+'_id']);
             $("#malRating").attr("href", anime['malurl']);
             if(isNaN(anime['.add_'+listType+'[status]'])){
-                $('#MalLogin').css("display","none");
+                $('.MalLogin').css("display","none");
                 $("#malRating").after("<span id='AddMalDiv'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='#' id='AddMal' onclick='return false;'>Add to Mal</a></span>")
                 $('#AddMal').click(function() {
                     var anime = {};
@@ -893,7 +893,7 @@ if (window.top != window.self) {return; }
                 }
                 //#############
             }
-            $("#MalData").css("display","initial");
+            $("#MalData").css("display","flex");
             $("#MalInfo").text("");
 
             getcommondata(anime['malurl']);
@@ -1716,44 +1716,23 @@ if (window.top != window.self) {return; }
         }
 
         $.docReady(function() {
-            if(listType == 'anime'){
-                var spacer = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp';
-                var middle = '';
-                middle += '<span class="info">Episodes: </span>';
-                middle += '<span style="color: '+textColor+'; text-decoration: none; outline: medium none;">';
-                middle += '<input id="malEpisodes" value="0" style="background: transparent; border-width: 1px; border-color: grey; text-align: right; color: '+textColor+'; text-decoration: none; outline: medium none;" type="text" size="1" maxlength="4">';
-                middle += '/<span id="malTotal">0</span>';
-                middle += '</span>';
+            var wrapStart = '<span style="display: inline-block;">';
+            var wrapEnd = '</span>';
 
-            }else{
-                var spacer = '&nbsp;&nbsp;&nbsp;&nbsp';
-                var middle = '';
-                middle += '<span class="info">Volumes: </span>';
-                middle += '<span style="color: '+textColor+'; text-decoration: none; outline: medium none;">';
-                middle += '<input id="malVolumes" value="0" style="background: transparent; border-width: 1px; border-color: grey; text-align: right; color: '+textColor+'; text-decoration: none; outline: medium none;" type="text" size="1" maxlength="4">';
-                middle += '/<span id="malTotalVol">0</span>';
-                middle += '</span>';
-
-                middle += spacer;
-
-                middle += '<span class="info">Chapters: </span>';
-                middle += '<span style="color: '+textColor+'; text-decoration: none; outline: medium none;">';
-                middle += '<input id="malChapters" value="0" style="background: transparent; border-width: 1px; border-color: grey; text-align: right; color: '+textColor+'; text-decoration: none; outline: medium none;" type="text" size="1" maxlength="4">';
-                middle += '/<span id="malTotalCha">0</span>';
-                middle += '</span>';
-            }
             var ui = '<p id="malp">';
             ui += '<span id="MalInfo">Loading</span>';
 
-            ui += '<span id="MalData" style="display: none;">';
+            ui += '<span id="MalData" style="display: none; justify-content: space-between;">';
 
+            ui += wrapStart;
             ui += '<span class="info">Mal Score: </span>';
             ui += '<a id="malRating" style="color: '+textColor+';min-width: 30px;display: inline-block;" target="_blank" href="">____</a>';
+            ui += wrapEnd;
 
-            ui += '<span id="MalLogin">';
+            //ui += '<span id="MalLogin">';
+            wrapStart = '<span style="display: inline-block; display: none;" class="MalLogin">';
 
-            ui += spacer;
-
+            ui += wrapStart;
             ui += '<span class="info">Status: </span>';
             ui += '<select id="malStatus" style="font-size: 12px;background: transparent; border-width: 1px; border-color: grey; color: '+textColor+'; text-decoration: none; outline: medium none;">';
             //ui += '<option value="0" style="background: #111111;color: '+textColor+';"></option>';
@@ -1763,15 +1742,43 @@ if (window.top != window.self) {return; }
             ui += '<option value="4" style="background: #111111;color: '+textColor+';">Dropped</option>';
             ui += '<option value="6" style="background: #111111;color: '+textColor+';">'+planTo+'</option>';
             ui += '</select>';
+            ui += wrapEnd;
 
-            ui += spacer;
+            if(listType == 'anime'){
+                var middle = '';
+                middle += wrapStart;
+                middle += '<span class="info">Episodes: </span>';
+                middle += '<span style="color: '+textColor+'; text-decoration: none; outline: medium none;">';
+                middle += '<input id="malEpisodes" value="0" style="background: transparent; border-width: 1px; border-color: grey; text-align: right; color: '+textColor+'; text-decoration: none; outline: medium none;" type="text" size="1" maxlength="4">';
+                middle += '/<span id="malTotal">0</span>';
+                middle += '</span>';
+                middle += wrapEnd;
+
+            }else{
+                var middle = '';
+                middle += wrapStart;
+                middle += '<span class="info">Volumes: </span>';
+                middle += '<span style="color: '+textColor+'; text-decoration: none; outline: medium none;">';
+                middle += '<input id="malVolumes" value="0" style="background: transparent; border-width: 1px; border-color: grey; text-align: right; color: '+textColor+'; text-decoration: none; outline: medium none;" type="text" size="1" maxlength="4">';
+                middle += '/<span id="malTotalVol">0</span>';
+                middle += '</span>';
+                middle += wrapEnd;
+
+
+                middle += wrapStart;
+                middle += '<span class="info">Chapters: </span>';
+                middle += '<span style="color: '+textColor+'; text-decoration: none; outline: medium none;">';
+                middle += '<input id="malChapters" value="0" style="background: transparent; border-width: 1px; border-color: grey; text-align: right; color: '+textColor+'; text-decoration: none; outline: medium none;" type="text" size="1" maxlength="4">';
+                middle += '/<span id="malTotalCha">0</span>';
+                middle += '</span>';
+                middle += wrapEnd;
+            }
 
             ui += middle;
 
-            ui += spacer;
 
+            ui += wrapStart;
             ui += '<span class="info">Your Score: </span>';
-
             ui += '<select id="malUserRating" style="font-size: 12px;background: transparent; border-width: 1px; border-color: grey; color: '+textColor+'; text-decoration: none; outline: medium none;"><option value="" style="background: #111111;color: '+textColor+';">Select</option>';
             ui += '<option value="10" style="background: #111111;color: '+textColor+';">(10) Masterpiece</option>';
             ui += '<option value="9" style="background: #111111;color: '+textColor+';">(9) Great</option>';
@@ -1784,7 +1791,9 @@ if (window.top != window.self) {return; }
             ui += '<option value="2" style="background: #111111;color: '+textColor+';">(2) Horrible</option>';
             ui += '<option value="1" style="background: #111111;color: '+textColor+';">(1) Appalling</option>';
             ui += '</select>';
-            ui += '</span>';
+            ui += wrapEnd;
+
+            //ui += '</span>';
             ui += '</span>';
             ui += '</p>';
 
