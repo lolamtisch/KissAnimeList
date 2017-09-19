387f7b7f8dd41eafa4a2803964b3878bd37dd6ed
lolamtisch <francisco.seipel@yahoo.de>
2017-09-19 21:45:33 +0200

    [FEATURE] Implement alternative tag system

--- src/code/09_malBookmarks.js
+++ src/code/09_malBookmarks.js
@@ -14,32 +14,72 @@
                     $('#list_surround table .animetitle').addClass("link");
                     $('#list_surround table td[class^="td"]:nth-last-child(2)').addClass("progress").addClass("data").find('span a').addClass('link');
                     //
-                    $('.tags span a').each(function( index ) {
-                        if($(this).text().indexOf("last::") > -1 ){
-                            url = $(this).text().split("last::")[1].split("::")[0];
-                            if(url.indexOf("masterani.me") > -1 && url.indexOf("/watch/") > -1){
-                                url = url.replace('/watch/','/info/');
-                            }
-                            var icon = '<img src="https://www.google.com/s2/favicons?domain='+url.split('/')[2]+'">'
-                            $(this).closest('.list-table-data').find('.data.title .link').after('<a class="stream" title="'+url.split('/')[2]+'" target="_blank" style="margin: 0 5px;" href="'+url+'">'+icon+'</a>');
-
-
-                            if(parseInt($(this).closest('.list-table-data').find('.data.progress .link').text())+1 == GM_getValue( url+'/next')){
-                                if(typeof GM_getValue( url+'/nextEp') != 'undefined'){
-                                    $(this).closest('.list-table-data').find('.stream').after('<a class="nextStream" title="Next Episode" target="_blank" style="margin: 0 5px 0 0; color: #BABABA;" href="'+ GM_getValue( url+'/nextEp')+'">'+'<img src="https://raw.githubusercontent.com/lolamtisch/KissAnimeList/master/Screenshots/if_Double_Arrow_Right_1063903.png" width="16" height="16">'+'</a>');
+                    if( $('.header-title.tags').height() || $('.td1.tags').height()){
+                        $('.tags span a').each(function( index ) {
+                            if($(this).text().indexOf("last::") > -1 ){
+                                url = $(this).text().split("last::")[1].split("::")[0];
+                                setStreamLinks(url, $(this).closest('.list-table-data'));
+                                if($('#list_surround').length){
+                                    $(this).remove();
+                                }else{
+                                    $(this).parent().remove();
                                 }
                             }
-                            if($('#list_surround').length){
-                                $(this).remove();
-                            }else{
-                                $(this).parent().remove();
-                            }
-                        }
-                    });
+                        });
+                    }else{
+                        alternativTagToContinue();
+                    }
+
                     return true;
                 }
             }, 300);
         });
+    }
+
+    function alternativTagToContinue(){
+        var user = window.location.href.split('/')[4].split('?')[0];
+        var listType = window.location.href.split('.net/')[1].split('list')[0];
+        url = "https://myanimelist.net/malappinfo.php?u="+user+"&status=all&type="+listType;
+        con.log("XML Url:", url);
+//TODO Check if tag system activated
+//TODO cache
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: url,
+            synchronous: false,
+            headers: {
+                "User-Agent": "Mozilla/5.0"
+            },
+            onload: function(response) {
+                con.log(response);
+                var xml = $(response.responseXML);
+                var title = '';
+                var xmlAnime = '';
+                $('.list-table-data').each(function( index ) {
+                    title = $(this).find('.title .link').text();
+                    xmlAnime = xml.find('series_title:contains('+title+')').first().parent();
+                    if(xmlAnime.find('my_tags').text().indexOf("last::") > -1 ){
+                        url = xmlAnime.find('my_tags').text().split("last::")[1].split("::")[0];
+                        setStreamLinks(url, $(this));
+                    }
+                });
+            }
+        });
+    }
+
+    function setStreamLinks(url, tableData){
+        if(url.indexOf("masterani.me") > -1 && url.indexOf("/watch/") > -1){
+            url = url.replace('/watch/','/info/');
+        }
+        var icon = '<img src="https://www.google.com/s2/favicons?domain='+url.split('/')[2]+'">'
+        $(tableData).find('.data.title .link').after('<a class="stream" title="'+url.split('/')[2]+'" target="_blank" style="margin: 0 5px;" href="'+url+'">'+icon+'</a>');
+
+
+        if(parseInt($(tableData).find('.data.progress .link').text())+1 == GM_getValue( url+'/next')){
+            if(typeof GM_getValue( url+'/nextEp') != 'undefined'){
+                $(tableData).find('.stream').after('<a class="nextStream" title="Next Episode" target="_blank" style="margin: 0 5px 0 0; color: #BABABA;" href="'+ GM_getValue( url+'/nextEp')+'">'+'<img src="https://raw.githubusercontent.com/lolamtisch/KissAnimeList/master/Screenshots/if_Double_Arrow_Right_1063903.png" width="16" height="16">'+'</a>');
+            }
+        }
     }
 
     function detailsPopup(){
