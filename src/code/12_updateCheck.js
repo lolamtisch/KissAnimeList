7a9ed9a5d49e99fa9611f5f070770879f53dc38c
lolamtisch <francisco.seipel@yahoo.de>
2017-11-03 15:36:48 +0100

    [TASK] Add episode selectors

--- src/code/12_updateCheck.js
+++ src/code/12_updateCheck.js
@@ -5,7 +5,26 @@
 			bookXML.find('my_status:contains(1)').parent().each(function(){
 				if($(this).find('my_tags').first().text().indexOf("last::") > -1 ){
 					var url = atobURL( $(this).find('my_tags').first().text().split("last::")[1].split("::")[0] );
-					var title = $(this).find('series_title').first().text()
+					var title = $(this).find('series_title').first().text();
+					var selector = '';
+
+					if( url.indexOf("kissanime.ru") > -1 ){
+					    selector = ".listing a";
+					}else if( url.indexOf("kissmanga.com") > -1 ){
+					    selector = ".listing a";
+					}else if( url.indexOf("masterani.me") > -1 ){
+						return true;//TODO
+					    selector = ".thumbnail a.title";
+					}else if( url.indexOf("9anime.to") > -1 ){
+					    selector = "#servers .episodes:first a";
+					}else if( url.indexOf("crunchyroll.com") > -1 ){
+					    selector = "#showview_content_videos .list-of-seasons .group-item a";
+					}else if( url.indexOf("gogoanime.") > -1 ){
+						return true;//TODO
+					    selector = "#episode_related a";
+					}
+
+
 					setTimeout( function(){
 						con.log('[EpCheck]', title, url );
 						GM_xmlhttpRequest({//TODO: Cloudflare handling
@@ -14,9 +33,9 @@
 							synchronous: false,
 							onload: function(response) {
 								var parsed  = $.parseHTML(response.response);
-								var EpNumber = $(parsed).find(".listing a").length;
+								var EpNumber = $(parsed).find( selector ).length;
 								con.log('[EpCheck]', GM_getValue('newEp_'+url+'_number',null), EpNumber);
-								if( GM_getValue('newEp_'+url+'_number', EpNumber) != EpNumber){
+								if( GM_getValue('newEp_'+url+'_number', EpNumber) < EpNumber){
 									con.log('[NewEP]', url);
 									alert('change');
 								}
@@ -26,8 +45,6 @@
 
 					}, time);
 					time += 5000;
-					return false;
-
 				}
 			});
 		});
