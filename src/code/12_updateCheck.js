6e2880c245b44b30e372e26cd7f260b6a2e9a25a
lolamtisch <francisco.seipel@yahoo.de>
2017-11-03 15:03:07 +0100

    [TASK] Add Episode number check and timeout

--- src/code/12_updateCheck.js
+++ src/code/12_updateCheck.js
@@ -1,20 +1,33 @@
 	function checkForNewEpisodes(){
+		var time = 0;
+
 		getMalXml("", function(bookXML){
 			bookXML.find('my_status:contains(1)').parent().each(function(){
 				if($(this).find('my_tags').first().text().indexOf("last::") > -1 ){
 					var url = atobURL( $(this).find('my_tags').first().text().split("last::")[1].split("::")[0] );
-					con.log('[EpCheck]', $(this).find('series_title').first().text(), url );
+					var title = $(this).find('series_title').first().text()
+					setTimeout( function(){
+						con.log('[EpCheck]', title, url );
+						GM_xmlhttpRequest({//TODO: Cloudflare handling
+							method: "GET",
+							url: url,
+							synchronous: false,
+							onload: function(response) {
+								var parsed  = $.parseHTML(response.response);
+								var EpNumber = $(parsed).find(".listing a").length;
+								con.log('[EpCheck]', GM_getValue('newEp_'+url+'_number',null), EpNumber);
+								if( GM_getValue('newEp_'+url+'_number', EpNumber) != EpNumber){
+									con.log('[NewEP]', url);
+									alert('change');
+								}
+								GM_setValue('newEp_'+url+'_number', EpNumber);
+							}
+						});
 
-					GM_xmlhttpRequest({//TODO: Cloudflare handling
-						method: "GET",
-						url: url,
-						synchronous: false,
-						onload: function(response) {
-							var parsed  = $.parseHTML(response.response);
-							console.log($(parsed).find(".listing a"));
-						}
-					});
+					}, time);
+					time += 5000;
 					return false;
+
 				}
 			});
 		});
