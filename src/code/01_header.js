c4f6ce64a8d10237348fe0eaede5d824e81298a1
lolamtisch <francisco.seipel@yahoo.de>
2017-11-12 16:27:33 +0100

    [VERSION]0.90.2

--- src/code/01_header.js
+++ src/code/01_header.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        KissAnimeList
-// @version     0.90.1
+// @version     0.90.2
 // @description Integrates MyAnimeList into various sites, with auto episode tracking.
 // @author      lolamtisch@gmail.com
 // @license 	CC-BY-4.0; https://creativecommons.org/licenses/by/4.0/legalcode
