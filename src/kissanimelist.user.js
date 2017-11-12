a109e3b3cebdd5b8acdee0011d4749340b929693
lolamtisch <francisco.seipel@yahoo.de>
2017-11-12 17:27:50 +0100

    [TASK] Update kissanimelist.user.js

--- src/kissanimelist.user.js
+++ src/kissanimelist.user.js
@@ -1977,6 +1977,7 @@
     }
 
     function flashConfirm(message, yesCall, cancelCall){
+        $('.flashPerm').remove();
         var rNumber = Math.floor((Math.random() * 1000) + 1);
         message = '<div style="text-align: left;">' + message + '</div><div style="display: flex; justify-content: space-around;"><button class="Yes'+rNumber+'" style="background-color: transparent; border: none; color: rgb(255,64,129);margin-top: 10px; cursor:pointer;">OK</button><button class="Cancel'+rNumber+'" style="background-color: transparent; border: none; color: rgb(255,64,129);margin-top: 10px; cursor:pointer;">CANCEL</button></div>';
         flashm(message, false, false, true);
