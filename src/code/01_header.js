01e47442cbb21a953b7dbe10cadb4e70ebf649aa
lolamtisch <francisco.seipel@yahoo.de>
2017-12-08 14:36:05 +0100

    [TASK] Add update and download url

--- src/code/01_header.js
+++ src/code/01_header.js
@@ -6,6 +6,8 @@
 // @license 	CC-BY-4.0; https://creativecommons.org/licenses/by/4.0/legalcode
 // @license 	MIT
 // @supportURL  https://github.com/lolamtisch/KissAnimeList/issues
+// @downloadURL https://greasyfork.org/scripts/27564-kissanimelist/code/KissAnimeList.user.js
+// @updateURL	https://greasyfork.org/scripts/27564-kissanimelist/code/KissAnimeList.meta.js
 // @include     http://kissanime.ru/Anime/*
 // @include     http://kissanime.to/Anime/*
 // @include     http://kissanime.ru/BookmarkList
