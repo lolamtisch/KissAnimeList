475c0fa127b322f063ec62276338701685b3daf0
lolamtisch <francisco.seipel@yahoo.de>
2017-12-14 10:57:40 +0100

    [TASK] Change floatbutton color to blue

--- src/code/11_miniMAL.js
+++ src/code/11_miniMAL.js
@@ -14,7 +14,7 @@
 
             GM_addStyle('.modal-content.fullscreen{width: 100% !important;height: 100% !important; bottom: 0 !important;'+( posLeft ? 'left':'right')+': 0 !important;}\
                          .modal-content{-webkit-transition: all 0.5s ease; -moz-transition: all 0.5s ease; -o-transition: all 0.5s ease; transition: all 0.5s ease;}\
-                         .floatbutton:hover {background-color:rgb(255,64,129);}\
+                         .floatbutton:hover {background-color:rgb(63,81,181);}\
                          .floatbutton:hover div {background-color:white;}\
                          .floatbutton div {background-color:black;-webkit-transition: all 0.5s ease;-moz-transition: all 0.5s ease;-o-transition: all 0.5s ease;transition: all 0.5s ease;}\
                          .floatbutton {\
