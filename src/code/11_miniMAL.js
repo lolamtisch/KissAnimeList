54dbd1aafbef4541b99ddf7a67f5ec5c0f567cff
lolamtisch <francisco.seipel@yahoo.de>
2018-01-28 15:17:10 +0100

    [BUGFIX] MiniMAL not displaying streaminglinks

--- src/code/11_miniMAL.js
+++ src/code/11_miniMAL.js
@@ -986,6 +986,7 @@
           continueHtml += '<div class="data title progress" style="display: inline-block; position: relative; top: 2px; margin-left: -2px;"><div class="link" style="display: none;">'+$("#info-iframe").contents().find('#myinfo_watchedeps').val()+'</div></div>';
           continueHtml +='</div>';
           getanime(url, function(actual){
+            try{
               if(actual['.add_anime[tags]'].indexOf("last::") > -1 ){
                   var url = atobURL( actual['.add_anime[tags]'].split("last::")[1].split("::")[0] );
                   $("#info-iframe").contents().find('.malDescription').first().append(continueHtml);
@@ -995,6 +996,7 @@
                   $("#info-iframe").contents().find('.malDescription .nextStream').append('Next Episode');
                   $("#info-iframe").contents().find('.malDescription .stream').append('Continue Watching');
               }
+            }catch(e) {console.log('[iframeOverview] Error:',e);}
           }, url, url.split('/')[3]);
         }catch(e) {console.log('[iframeOverview] Error:',e);}
 
