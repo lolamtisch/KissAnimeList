46bac0c295795def02b55171d3cf51b8263a4169
lolamtisch <francisco.seipel@yahoo.de>
2018-03-10 12:22:34 +0100

    [TASK] Add isInviewport helper

--- src/code/05_helper.js
+++ src/code/05_helper.js
@@ -426,3 +426,12 @@
             }
         });
     }
+
+    $.fn.isInViewport = function() {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+
